488c0f4a87cbc710819f84bf42b6d9b09f3c8cc4
Lana Gordon <37781167+Larlar03@users.noreply.github.com>
2021-12-16 19:44:11 +0000

    Added rel attribute to audio link

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -5,7 +5,7 @@ export default function Phonetics(props) {
   if (props.phonetic) {
     return (
       <div className="Phonetic">
-        <a href={props.phonetic.audio} target="_blank">
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
           Listen
         </a>
         <br />
